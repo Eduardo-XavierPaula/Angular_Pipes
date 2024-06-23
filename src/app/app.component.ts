e60329705041a07c22a85a85fd5fc4c90e5f171e
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-06-23 19:04:44 -0300

    ✨ New feature: Estudo de pipe date, by Eduardo Xavier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export enum UserStatusEnum {
   ATIVO = 1,
@@ -10,10 +10,19 @@ export enum UserStatusEnum {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   pessoa = {
     nome: 'Felipe',
     idade: 26,
     status: UserStatusEnum.ATIVO,
   };
+
+  minhaData: string = '2023-10-21T21:00:00.000Z';
+  minhaDataObj: Date = new Date('2023-10-21T21:00:00.000Z');
+  ngOnInit(): void {
+    console.log('Data Convertida para a região', new Date(this.minhaData));
+    console.log('Data UTC-0', new Date(this.minhaData).toUTCString());
+    console.log('Timestamp', new Date(this.minhaData).getTime());
+    console.log('Timestamp', this.minhaDataObj);
+  }
 }
