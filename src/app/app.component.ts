74573c186fc93e3b379c1f07a7de0144b587e291
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-06-22 22:54:56 -0300

    🎉 Initial commit: Estudo de Pipes no Angular, by Eduardo Xavier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export enum UserStatusEnum {
+  ATIVO = 1,
+  INATIVO = 2,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Angular_Pipes';
+  pessoa = {
+    nome: 'Felipe',
+    idade: 26,
+    status: UserStatusEnum.ATIVO,
+  };
 }
