4f64c45a9d7fd7fe999b153ca481373f2841492f
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-06-23 20:07:46 -0300

    ✨ New feature: Formatação padrão e localidade de pipe date, by Eduardo Xavier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { AppComponent } from './app.component';
 import { UserStatusPipe } from './pipes/user-status.pipe';
 import { TruncatePipe } from './pipes/truncate.pipe';
 
+import { DATE_PIPE_DEFAULT_OPTIONS, DatePipeConfig } from '@angular/common';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+import { LOCALE_ID } from '@angular/core';
+
+const datePipeConfig: DatePipeConfig = {
+  dateFormat: 'dd/MM/YYYY',
+  timezone: '+0000',
+};
+
+registerLocaleData(localePt, 'pt-BR');
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserStatusPipe,
-    TruncatePipe
+  declarations: [AppComponent, UserStatusPipe, TruncatePipe],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: datePipeConfig,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
